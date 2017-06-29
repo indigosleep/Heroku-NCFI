7e7e1d5b72df5cf6555821866baaaef6412a61ad
John Sawyer <sawyer@sawyermerchant.com>
2017-06-29 11:31:54 -0400

    Order controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'orders#index'
+
+  scope :api do
+    scope :v1 do
+      resources :orders
+    end
+  end
+
 end
